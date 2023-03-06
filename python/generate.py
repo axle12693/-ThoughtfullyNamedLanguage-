c6b8379a82f2492eb938a5fd84f8aa3933a10179
Alex Bentley <abentley@onetelcloud.com>
2023-03-06 16:05:38 -0500

    Refactor

--- python/generate.py
+++ python/generate.py
@@ -1,76 +1,45 @@
 import os
 
 
-def gencode(ast: list[dict]) -> None:
+def generate_c_code(ast):
     intdecs = set()
     mallocs = set()
-    code = generate_c_code(ast, intdecs, mallocs)
-
-    write_to_file('output.c', code)
-
-    if os.name != 'nt':
-        compile_c_file()
-
-    return None
-
-
-def generate_c_code(ast: list[dict], intdecs: set, mallocs: set) -> str:
     code = "#include <stdlib.h>\n#include <string.h>\n\nint main() {"
 
     for i in ast:
         if i['type'] == 'VariableDeclaration':
-            code = generate_variable_declaration_code(i, intdecs, mallocs, code)
-
+            if i['datatype'] == 'int':
+                if i['value'] not in intdecs:
+                    code += f"int {i['value']};"
+                    intdecs.add(i['value'])
+            elif i['datatype'] == 'str':
+                if i['value'] not in mallocs:
+                    code += f"char *{i['value']} = malloc(256);"
+                    mallocs.add(i['value'])
         elif i['type'] == 'VariableAssignment':
-            code = generate_variable_assignment_code(i, code)
+            if isinstance(i['value'], str):
+                code += f"strcpy({i['name']}, {i['value']});"
+            else:
+                code += f"{i['name']} = {i['value']};"
 
-    code = free_memory_allocated_by_mallocs(mallocs, code)
+    for var_name in mallocs:
+        code += f"free({var_name});"
 
     code += "return 0;}"
     return code
 
 
-def generate_variable_declaration_code(i: dict, intdecs: set, mallocs: set, code: str) -> str:
-    if i['datatype'] == 'int':
-        code = generate_int_variable_declaration_code(i, intdecs, code)
-    elif i['datatype'] == 'str':
-        code = generate_str_variable_declaration_code(i, mallocs, code)
-
-    return code
-
-
-def generate_int_variable_declaration_code(i: dict, intdecs: set, code: str) -> str:
-    if i['value'] not in intdecs:
-        code += f"int {i['value']};"
-        intdecs.add(i['value'])
-    return code
-
-
-def generate_str_variable_declaration_code(i: dict, mallocs: set, code: str) -> str:
-    if i['value'] not in mallocs:
-        code += f"char *{i['value']} = malloc(256);"
-        mallocs.add(i['value'])
-    return code
-
-
-def generate_variable_assignment_code(i: dict, code: str) -> str:
-    if isinstance(i['value'], str):
-        code += f"strcpy({i['name']}, {i['value']});"
-    else:
-        code += f"{i['name']} = {i['value']};"
-    return code
-
-
-def free_memory_allocated_by_mallocs(mallocs: set, code: str) -> str:
-    for var_name in mallocs:
-        code += f"free({var_name});"
-    return code
-
-
-def write_to_file(filename: str, code: str) -> None:
+def write_to_file(filename, code):
     with open(filename, 'w') as cfile:
         cfile.write(code)
 
 
-def compile_c_file() -> None:
-    os.system("cc output.c")
+def compile_c_file():
+    if os.name != 'nt':
+        os.system("cc output.c")
+
+
+def gencode(ast):
+    code = generate_c_code(ast)
+    write_to_file('output.c', code)
+    compile_c_file()
