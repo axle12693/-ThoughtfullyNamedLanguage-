7fa5fbf5700cec18b033b4a32f089970e399ee99
sphericalcylinder <Jonahposner@icloud.com>
2023-03-11 09:55:31 -0500

    largefile updated with operations
    such largeability

--- c++/output.c
+++
@@ -1,4 +0,0 @@
-#include <stdlib.h>
-#include <string.h>
-
-int main() {int x;x = 5;x = 2 + 3;char *y = (char*)malloc(256);char *a = (char*)malloc(256);strcpy(y, "lol");strcpy(a, "lamoloserwannabe");free(y);free(a);return 0;}
